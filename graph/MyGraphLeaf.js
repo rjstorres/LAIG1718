59dba6ade47909402e3352492da3aaff5a331ec1
MT <miguelangelo_teixeira@hotmail.com>
2017-10-02 11:55:35 +0100

    Display function. Added leaf properties

--- graph/MyGraphLeaf.js
+++ graph/MyGraphLeaf.js
@@ -4,5 +4,30 @@
 **/
 
 function MyGraphLeaf(graph, xmlelem) {
+	this.graph = graph;
+	this.args = graph.reader.getString(xmlelem, 'args');
+	this.type = graph.reader.getString(xmlelem, 'type');
+	this.primitive = null;
+
+	switch (this.type) {
+		case "triangle":
+			this.primitive = new Triangle(this.graph.scene, this.args);
+			break;
+		case 'sphere':
+			this.primitive = new Sphere(this.graph.scene, this.args);
+			break;
+		case 'cylinder':
+			this.primitive = new Cylinder(this.graph.scene, this.args);
+			break;
+		case 'rectangle':
+			this.primitive = new Rectangle(this.graph.scene, this.args);
+			break;
+		default:
+			break;
+	}
+
 }
 
+MyGraphLeaf.prototype.display = function(){
+	this.primitive.display();
+}
