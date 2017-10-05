b55ae6bad2d1e50bb6a77224eb38e77f1a3e67f9
MT <miguelangelo_teixeira@hotmail.com>
2017-10-05 23:52:35 +0100

    Colocar coord Texturas nas primitivas

--- graph/MyGraphLeaf.js
+++ graph/MyGraphLeaf.js
@@ -7,7 +7,6 @@ function MyGraphLeaf(graph, xmlelem) {
 	this.graph = graph;
 	this.args = graph.reader.getString(xmlelem, 'args');
 	this.args = this.args.split(" ").map(Number);
-	console.log(this.args);
 	this.type = graph.reader.getString(xmlelem, 'type');
 	this.primitive = null;
 
@@ -30,6 +29,7 @@ function MyGraphLeaf(graph, xmlelem) {
 
 }
 
-MyGraphLeaf.prototype.display = function(){
+MyGraphLeaf.prototype.display = function(s, t){
+	this.primitive.setTextureCoords(s,t);
 	this.primitive.display();
 }
