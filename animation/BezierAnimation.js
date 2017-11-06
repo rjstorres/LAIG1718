ae783e7bbae5f8746915171cba86f47af248e6f9
rjstorres <r_torres96@live.com.pt>
2017-11-06 12:02:33 +0000

    started bezier

--- animation/BezierAnimation.js
+++ animation/BezierAnimation.js
@@ -3,6 +3,13 @@
  * @constructor
  */
 function BezierAnimation(scene, args) {
+    (args.length == 5 && args[0] === Array && args[1] === Array && args[2] === Array && 
+        args[3] === Array && args[4] === Number) ? null : console.log("Error");
+    this.P1=args[0];    
+    this.P2=args[1];
+    this.P3=args[2];
+    this.P4=args[3];
+    this.speed=args[4];
 };
 
 BezierAnimation.prototype = Object.create(Animation.prototype);
