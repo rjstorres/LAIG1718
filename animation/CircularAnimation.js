a658bbb30f0184e519d83871d0b6f6f2965b457e
rjstorres <r_torres96@live.com.pt>
2017-11-06 10:38:34 +0000

    started circular animation

--- animation/CircularAnimation.js
+++ animation/CircularAnimation.js
@@ -3,6 +3,13 @@
  * @constructor
  */
 function CircularAnimation(scene, args) {
+    (args.length == 5 && args[0] === Array && args[1] === Number && 
+        args[2] === Number && args[3] === Number && args[4] === Number) ? null : console.log("Error");
+    this.center = args[0];
+    this.radius = args[1];
+    this.initialAngle = args[2];
+    this.rotationAngle = args[3];
+    this.speed = args[4];
 };
 
 CircularAnimation.prototype = Object.create(Animation.prototype);
