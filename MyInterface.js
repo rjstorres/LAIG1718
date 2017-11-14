ceec2b83d8ec1891b393a38e82ed569df35dbe56
MT <miguelangelo_teixeira@hotmail.com>
2017-11-14 10:35:50 +0000

    Função para gerar uma listbox com nós selecionaveis

--- MyInterface.js
+++ MyInterface.js
@@ -3,7 +3,7 @@
  * @constructor
  */
 function MyInterface() {
-    //call CGFinterface constructor 
+    //call CGFinterface constructor
     CGFinterface.call(this);
 }
 ;
@@ -21,11 +21,11 @@ MyInterface.prototype.init = function(application) {
 
     // init GUI. For more information on the methods, check:
     //  http://workshop.chromeexperiments.com/examples/gui
-    
+
     this.gui = new dat.GUI();
 
     // add a group of controls (and open/expand by defult)
-    
+
     return true;
 };
 
@@ -48,3 +48,11 @@ MyInterface.prototype.addLightsGroup = function(lights) {
     }
 }
 
+MyInterface.prototype.addSelectableGroup = function(nodes){
+  var group = this.gui.addFolder("Selectables");
+  group.open();
+  for (var i = 0; i < nodes.length; i++) {
+      this.scene.selectableNodes[nodes[i]] = false;
+      group.add(this.scene.selectableNodes, nodes[i]);
+  }
+}
